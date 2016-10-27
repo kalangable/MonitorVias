57ceea935bfbb76ab68dd8c4a68824caabf8111c
kalangable <kalangable@gmail.com>
2016-10-27 02:35:23 -0200

    Inclusão de rotina para calcular a media durante a leitura

--- MonitorVias.h
+++ MonitorVias.h
@@ -11,12 +11,17 @@ GPS gps;
 GravarSD cartao;
 Formatador formato;
 
+#define elementosMedicao 10
+
 double velocidadeMinima = 7.0;
 
 double lon,lat;
 
 bool isGravar( double latitude, double longitude,   double velocidade);
-
 bool setResposta( double latitude, double longitude , bool resposta );
+bool generatedInfoZ( int eixoZ );
+short lista[elementosMedicao] = { 760, 810 , 710 , 760 , 810 , 710 , 760 , 810 , 710 , 760};
+int media = 760;
+
 
 #endif
--- MonitorVias.ino
+++ MonitorVias.ino
@@ -14,12 +14,13 @@ void setup() {
 void loop() {
   
   acelerometro.getInformacoes();
-  gps.novasCoordenadas();
-  if( isGravar( gps.getLatitude(), gps.getLongitude(), gps.getVelocidade() ) ){
-    formato.setDados( acelerometro.getEixoX(), acelerometro.getEixoY(), acelerometro.getEixoZ(), gps.getLongitude(), gps.getLatitude(), gps.getVelocidade(), gps.getTime() );
-    cartao.gravar( formato.csv() );
+  if( generatedInfoZ( acelerometro.getEixoZ() )){
+    gps.novasCoordenadas();
+    if( isGravar( gps.getLatitude(), gps.getLongitude(), gps.getVelocidade() ) ){
+      formato.setDados( acelerometro.getEixoX(), acelerometro.getEixoY(), acelerometro.getEixoZ(), gps.getLongitude(), gps.getLatitude(), gps.getVelocidade(), gps.getTime() );
+      cartao.gravar( formato.csv() );
+    }
   }
-  
 }
 
 bool isGravar( double latitude, double longitude, double velocidade){
@@ -39,3 +40,19 @@ bool setResposta( double latitude, double longitude , bool resposta ){
   lat = latitude;
   return resposta;
 }
+
+bool generatedInfoZ( int eixoZ ){
+  
+  int soma = eixoZ;
+  double somaDesvioPadrao = pow( float( media - lista[0] ), float(2));
+  for( int contador=1; contador < elementosMedicao; contador++){
+    lista[contador-1] = lista[contador];
+    soma+= lista[contador];
+    somaDesvioPadrao+= pow( float(media-lista[contador]), float(2));
+  }
+  lista[elementosMedicao-1] = eixoZ;
+  double desvioPadrao = sqrt( somaDesvioPadrao/(elementosMedicao-1) );
+  media = soma/elementosMedicao;
+  return ( ( ( lista[elementosMedicao-2] - desvioPadrao ) < eixoZ ) && ( ( lista[elementosMedicao-2] + desvioPadrao ) > eixoZ) ) ;
+}
+
